4bc760e96e28df5d82f21e1c587c703f271c7bfb
LIMON100 <mahmudurlimon41@gmail.com>
2023-10-08 16:46:34 +0600

    update state

---
+++ Kalman Filter/uncented kalman filter/Practice/update_state/main.cpp
@@ -0,0 +1,18 @@
+#include "Dense"
+#include "ukf.h"
+#include "ukf.cpp"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+int main() {
+
+  // Create a UKF instance
+  UKF ukf;
+
+  VectorXd x_out = VectorXd(5);
+  MatrixXd P_out = MatrixXd(5, 5);
+  ukf.UpdateState(&x_out, &P_out);
+
+  return 0;
+}
