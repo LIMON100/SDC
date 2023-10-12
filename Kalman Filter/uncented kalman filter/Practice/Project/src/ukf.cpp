08e54ff82a46dddf4e51e361d2ffb11374388f25
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-10-12 10:30:06 +0600

    complete preprocess and initialize

--- Kalman Filter/uncented kalman filter/Practice/Project/src/ukf.cpp
+++ Kalman Filter/uncented kalman filter/Practice/Project/src/ukf.cpp
@@ -1,9 +1,11 @@
 #include "ukf.h"
 #include "Eigen/Dense"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+using namespace std;
 /**
  * Initializes Unscented Kalman filter
  */
@@ -78,11 +80,6 @@ UKF::UKF() {
   /**
    * End DO NOT MODIFY section for measurement noise values 
    */
-  
-  /**
-   * TODO: Complete the initialization. See ukf.h for other member properties.
-   * Hint: one or more values initialized above might be wildly off...
-   */
 }
 
 UKF::~UKF() {}
@@ -92,6 +89,74 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    * TODO: Complete this function! Make sure you switch between lidar and radar
    * measurements.
    */
+  bool bDebug = false;
+  if(!is_initialized_){
+    if(meas_package.sensor_type_ == MeasurementPackage::LASER){
+      x_ << meas_package.raw_measurements_[0],
+            meas_package.raw_measurements_[1],
+            0,
+            0,
+            0;
+
+      P_ << std_laspx_ * std_laspx_, 0, 0, 0,
+            0, std_laspy_ * std_laspy_, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, 1, 0,
+            0, 0, 1, 0, 1;
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::RADAR){
+      double rho = meas_package.raw_measurements_[0];
+      double phi = meas_package.raw_measurements_[1];
+      double rho_dot = meas_package.raw_measurements_[2];
+
+      x_ << rho * cos(phi),
+            rho * sin(phi),
+            0,
+            0,
+            0;
+      
+      P_ << std_radr_*std_radr_, 0, 0, 0, 0,
+          0, std_radr_ * std_radr_, 0, 0, 0,
+          0, 0, std_radrd_ * std_radrd_, 0, 0,
+          0, 0, 0, std_radphi_ * std_radphi_, 0,
+          0, 0, 0, 0, 1;
+    }
+    else{
+      std::cout << "UKF::ProcessMeasurement() error: cannot initialize because of invalid measurement sensor type " << meas_package.sensor_type_<< std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    time_us_ = meas_package.timestamp_;
+    is_initialized_ = true;
+
+    if (bDebug){
+      std::cout << "UKF::ProcessMeasurement() initializes for sensor type " << meas_package.sensor_type_ << " - state vector x:\n" << x_ << std::endl;
+    }
+    return;
+  }
+
+  // compute the time elapsed between the current and previous measurements dt in seconds
+  double dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+  time_us_ = meas_package.timestamp_;
+
+  Prediction(dt);
+  if (bDebug)
+    std::cout << "UKF::ProcessMeasurement() predicts the state vector x:\n" << x_ << std::endl;
+
+  if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+    UpdateLidar(meas_package);
+    if (bDebug)
+      std::cout << "UKF::ProcessMeasurement() Lidar update - state vector x:\n" << x_ << std::endl;
+  }
+  else if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    UpdateRadar(meas_package);
+    if (bDebug)
+      std::cout << "UKF::ProcessMeasurement() Radar update - state vector x:\n" << x_ << std::endl;
+  }
+  else {
+    std::cerr << "UKF::ProcessMeasurement() error: cannot update measurement because of invalid measurement sensor type " << meas_package.sensor_type_ << std::endl;
+    exit(EXIT_FAILURE);
+  }
 }
 
 void UKF::Prediction(double delta_t) {
